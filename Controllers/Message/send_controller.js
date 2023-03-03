ac2d2802c56ba1b888a4f4f509c81d7b4c257667
Adeoye John Oluwakayode <johnfixit293@gmail.com>
2023-03-03 17:26:42 +0100

    working on the uploading of video

--- Controllers/Message/send_controller.js
+++ Controllers/Message/send_controller.js
@@ -3,28 +3,44 @@ const { uploadAudio } = require("../FileUpload/audio");
 
 
 module.exports.sendMsg=(req, res)=>{
-    const {category, text, audioFile, video, location} = req.body;
+    const {category, text, audioFile, videoFile, location} = req.body;
     
-    if(!!audioFile){
-        uploadAudio(audioFile).then((audioUploadRes)=>{
-            if(audioUploadRes === 'ENOTFOUND'){
-                res.status(500).json({message: "Network error, please check your connection", success: false})
-            }
-            else{
-                saveMsg({category, text, audioUploadRes, video, location}).then((saveRes)=>{
-                    res.status(saveRes.status).json({message: saveRes.message, success: saveRes.success})
-                })
-            }
+    // if(!!audioFile){
+    //     uploadAudio(audioFile).then((audioUploadRes)=>{
+    //         if(audioUploadRes === 'ENOTFOUND'){
+    //             res.status(500).json({message: "Network error, please check your connection", success: false})
+    //         }
+    //         else{
+    //             saveMsg({category, text, audioUploadRes, video, location}).then((saveRes)=>{
+    //                 res.status(saveRes.status).json({message: saveRes.message, success: saveRes.success})
+    //             })
+    //         }
+            
+    //     }).catch((err)=>{
+    //         res.status(500).json({message: `${err.message}, please check your connection`, success: false})
+    //     })
+    // }
+    if(!!videoFile){
+             uploadAudio(videoFile).then((uRes)=>{
+                console.log(uRes)
+            // if(audioUploadRes === 'ENOTFOUND'){
+            //     res.status(500).json({message: "Network error, please check your connection", success: false})
+            // }
+            // else{
+            //     saveMsg({category, text, audioUploadRes, video, location}).then((saveRes)=>{
+            //         res.status(saveRes.status).json({message: saveRes.message, success: saveRes.success})
+            //     })
+            // }
             
         }).catch((err)=>{
             res.status(500).json({message: `${err.message}, please check your connection`, success: false})
         })
     }
-    else{
-        saveMsg({category, text, video, location}).then((saveRes)=>{
-            res.status(saveRes.status).json({message: saveRes.message, success: saveRes.success})
-        })
-    }
+    // else{
+    //     saveMsg({category, text, video, location}).then((saveRes)=>{
+    //         res.status(saveRes.status).json({message: saveRes.message, success: saveRes.success})
+    //     })
+    // }
     
     
 }
@@ -36,7 +52,5 @@ const saveMsg=(data)=>{
         return {message: "Message sent", success: true, suggestedMeasure: "This will be writing according to the cateory of the emergency", status: 200}
     }).catch((err)=>{
         return {message: err.message, success: false, status: 500}
-    })
-    
+    })   
 }
-
