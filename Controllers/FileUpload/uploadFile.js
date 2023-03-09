cef28fe85e5f9bb1c8880219fa4863a72d9ee452
Adeoye John Oluwakayode <johnfixit293@gmail.com>
2023-03-09 23:21:16 +0100

    working on uploading of audio and video together if both were to be uploaded

--- Controllers/FileUpload/uploadFile.js
+++ Controllers/FileUpload/uploadFile.js
@@ -2,10 +2,24 @@ const { cloudinaryConfig } = require("../../CloudinaryConfig")
 cloudinaryConfig()
 const cloudinary = require("cloudinary").v2;
 let timeStamp = new Date().getTime();
-module.exports.uploadFile=(file)=>{
-    let fileForm = file.split(";")[0].split(":")[1].split("/")[0]
+module.exports.uploadaFile=(file)=>{
+    let fileForm
+    if(!!file){
+       fileForm = file.split(";")[0].split(":")[1].split("/")[0]
+    }
     return cloudinary.uploader.upload(file, {folder: `emergency_system/${fileForm}_files/`, public_id: timeStamp, resource_type: "auto"}).then((res)=>{
-        return res.secure_url
+        return {fileForm, url: res.secure_url}
+    }).catch((err)=>{
+        return err.error.code
+    })
+}
+module.exports.uploadvFile=(file)=>{
+    let fileForm
+    if(!!file){
+       fileForm = file.split(";")[0].split(":")[1].split("/")[0]
+    }
+    return cloudinary.uploader.upload(file, {folder: `emergency_system/${fileForm}_files/`, public_id: timeStamp, resource_type: "auto"}).then((res)=>{
+        return {fileForm, url: res.secure_url}
     }).catch((err)=>{
         return err.error.code
     })
--- Controllers/Message/send_controller.js
+++ Controllers/Message/send_controller.js
@@ -1,54 +1,76 @@
 const { messageModel } = require("../../Models/messagesModel");
-const { uploadAudio } = require("../FileUpload/uploadFile");
+const { uploadvFile, uploadaFile } = require("../FileUpload/uploadFile");
 
 
-module.exports.sendMsg=(req, res)=>{
+module.exports.sendMsg=async(req, res)=>{
     const {category, text, audioFile, videoFile, location} = req.body;
+
+    if(!!audioFile || !!videoFile){
+        let details = {category, text, location, audio: "", video: ""}
+        await uploadaFile(audioFile).then((audioRes)=>{
+            if(audioRes === 'ENOTFOUND'){
+                console.log(audioRes)
+                res.status(500).json({message: "Network error, please check your connection", success: false})
+            }
+            details.audio= audioRes.url
+         })
+         
+         await uploadvFile(videoFile).then((videoRes)=>{
+            details.video= videoRes.url
+         })
+
+         saveMsg(details).then((saveRes)=>{
+            res.status(saveRes.status).json({message: saveRes.message, success: saveRes.success})
+        })
+
+    }
+
+
     
-    if(!!audioFile){
-        uploadAudio(audioFile).then((audioUploadRes)=>{
-            if(audioUploadRes === 'ENOTFOUND'){
-                res.status(500).json({message: "Network error, please check your connection", success: false})
-            }
-            else{
-                // saveMsg({category, text, audioUploadRes, video, location}).then((saveRes)=>{
-                //     res.status(saveRes.status).json({message: saveRes.message, success: saveRes.success})
-                // })
-            }
-            
-        })   
-    .catch((err)=>{
-            res.status(500).json({message: `${err.message}, please check your connection`, success: false})
-        })
-    }
-    if(!!videoFile){
-             uploadAudio(videoFile).then((videoUploadRes)=>{  
-            if(videoUploadRes === 'ENOTFOUND'){
-                res.status(500).json({message: "Network error, please check your connection", success: false})
-            }
-            else{
-                // saveMsg({category, text, audioUploadRes, video, location}).then((saveRes)=>{
-                //     res.status(saveRes.status).json({message: saveRes.message, success: saveRes.success})
-                // })
-            }
-            
-        }).catch((err)=>{
-            res.status(500).json({message: `${err.message}, please check your connection`, success: false})
-        })
-    }
-    // else{
-    //     saveMsg({category, text, video, location}).then((saveRes)=>{
-    //         res.status(saveRes.status).json({message: saveRes.message, success: saveRes.success})
+    // if(!!audioFile){
+    //     uploadFile(audioFile).then((audioUploadRes)=>{
+    //         if(audioUploadRes === 'ENOTFOUND'){
+    //             res.status(500).json({message: "Network error, please check your connection", success: false})
+    //         }
+    //         else{
+    //             saveMsg({category, text, audioUploadRes, video, location}).then((saveRes)=>{
+    //                 res.status(saveRes.status).json({message: saveRes.message, success: saveRes.success})
+    //             })
+    //         }
+    //     })   
+    // .catch((err)=>{
+    //         res.status(500).json({message: `${err.message}, please check your connection`, success: false})
     //     })
     // }
+    // if(!!videoFile){
+    //          uploadFile(videoFile).then((videoUploadRes)=>{  
+    //             console.log(videoUploadRes)
+    //         if(videoUploadRes === 'ENOTFOUND'){
+    //             res.status(500).json({message: "Network error, please check your connection", success: false})
+    //         }
+    //         else{
+    //             saveMsg({category, text, audioUploadRes, videoUploadRes, location}).then((saveRes)=>{
+    //                 res.status(saveRes.status).json({message: saveRes.message, success: saveRes.success})
+    //             })
+    //         }
+            
+    //     }).catch((err)=>{
+    //         res.status(500).json({message: `${err.message}, please check your connection`, success: false})
+    //     })
+    // }
+    else{
+        saveMsg({category, text, location}).then((saveRes)=>{
+            res.status(saveRes.status).json({message: saveRes.message, success: saveRes.success})
+        })
+    }
     
     
 }
 
 
 const saveMsg=(data)=>{
-    const {category, text, audioUploadRes, video, location} = data
-    return messageModel.create({category, message: {text, audio:audioUploadRes, video}, location}).then((data)=>{
+    const {category, text, audio, video, location} = data
+    return messageModel.create({category, message: {text, audio, video}, location}).then((data)=>{
         return {message: "Message sent", success: true, suggestedMeasure: "This will be writing according to the cateory of the emergency", status: 200}
     }).catch((err)=>{
         return {message: err.message, success: false, status: 500}
