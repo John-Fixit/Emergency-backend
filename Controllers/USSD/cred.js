f894a0484879fa4aba6ebe7b8219783486cba74f
Adeoye John Oluwakayode <johnfixit293@gmail.com>
2023-05-13 16:31:04 +0100

    adding ussd

---
+++ Controllers/USSD/cred.js
@@ -0,0 +1,14 @@
+module.exports = {
+    AT:{
+        apiKey: 'xxxxxxxxxxxxxxxxxxxxxxx',
+        username: 'xxxxxxx',
+        format: 'json'
+    },
+    pusher:{
+       appId: 'xxxxxx',
+       key: 'xxxxxxxxxxxxxxxx',
+       secret: "xxxxxxxxxxxxxx",
+       cluster: "xx",
+       encrypted: true
+    }
+   }
--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const bodyParser = require("body-parser");
 const { json } = require("express");
 const socket = require("socket.io");
 const { respondRouter } = require("./Routes/Message/respondRoute");
-
+const Pusher = require('pusher')
+const credentials = require('./Controllers/USSD/cred')
+const africastalking = require('africastalking')(credentials.AT)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(json({ limit: "100mb" }));
 app.use(cors({ origin: "*" }));
@@ -16,12 +18,33 @@ app.use(cors({ origin: "*" }));
 app.get("/", function (req, res) {
   res.status(200).json({ message: "Hello world", status: true });
 });
+app.post('/ussd/report', (req,res) => {
+  console.log(req.body)
+  //configure /order api
+})
+
 app.use("/org", orgRoute);
 app.use("/msg", messageRouter);
 app.use("/respond", respondRouter)
+
 //mongoDB connection
 connection();
+//pusher codes//
+const pusher = new Pusher(credentials.pusher)
+var webURL = 'http://foodigo.com/menu'
+var welcomeMsg = `CON Hello and welcome to Emergency notification system!
+Please find our menu ${webURL}
+Enter your name to continue`
+var orderDetails = {
+    name: "",
+    description: "",
+    address: "",
+    telephone: "",
+    open: true
+}
+var lastData = "";
 
+//
 const PORT = process.env.PORT || 4001;
 let server = app.listen(PORT, (req, res) => {
   console.log(`Server is responding on port: ${PORT}`);
