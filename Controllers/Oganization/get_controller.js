d13f1357fd5b384375e6eaf4a262cb4b1c43aa85
Adeoye John Oluwakayode <johnfixit293@gmail.com>
2023-05-13 14:22:12 +0100

    updated

--- Controllers/Oganization/get_controller.js
+++ Controllers/Oganization/get_controller.js
@@ -1,4 +1,5 @@
 const { orgModel } = require('../../Models/orgModel');
+const {StatusCodes} = require('http-status-codes')
 module.exports.getOrgDetail=(req, res)=>{
     const id = req.params.id;
     orgModel.find({id}, (err, data)=>{
@@ -10,4 +11,21 @@ module.exports.getOrgDetail=(req, res)=>{
         //    res.status(200).json({message: "Successfull", detail: data, success: true});
         }
     })
-}   
+}
+
+module.exports.getAllOrg=(req, res)=>{
+    orgModel.find((err, data)=>{
+        if(err){ 
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message: 'Unexpected error! please check your connection', success: false});
+        }
+        else{
+            let orgs = data?.map((org)=>{
+                return {
+                    name: org.name,
+                    mobile: org.mobile
+                }
+            })
+            res.status(StatusCodes.OK).json({message:'successfull', success: true, result: orgs});
+        }
+    })
+}
--- Routes/Organization/orgRoute.js
+++ Routes/Organization/orgRoute.js
@@ -2,10 +2,11 @@ const app = require("express");
 const orgRoute = app.Router();
 const {addOrg, login, authorize} = require("../../Controllers/Oganization/auth_controller");
 const { loginMiddleWare } = require("../../Middlewares/loginEmailMiddleware");
-const { getOrgDetail } = require("../../Controllers/Oganization/get_controller");
+const { getOrgDetail, getAllOrg } = require("../../Controllers/Oganization/get_controller");
 
 orgRoute.post("/addOrg", addOrg)
 orgRoute.post("/login", loginMiddleWare, login);
 orgRoute.get('/authorize', authorize)
 orgRoute.get('/profile/:id', getOrgDetail)
+orgRoute.get('/allOrgs', getAllOrg)
 module.exports = {orgRoute};
